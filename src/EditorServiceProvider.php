8db6d30c0e4668cd75b8087ead3e77c4e5dcf915
Nick Adams <nickolas.j.adams@gmail.com>
2022-04-10 17:46:22 -0600

    Try to fix issue with publishable views not being overwritten

--- src/EditorServiceProvider.php
+++ src/EditorServiceProvider.php
@@ -30,10 +30,12 @@ class EditorServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../public' => public_path('/'),
-        ], 'editor-public');
+            // __DIR__.'/../resources/assets' => resource_path('arcanely/editor/assets'),
+        ], 'editor-assets');
 
         $this->publishes([
-            __DIR__.'/../resources' => resource_path('arcanely/editor'),
+            // __DIR__.'/../resources' => resource_path('arcanely/editor'),
+            __DIR__.'/../resources/views' => resource_path('views/vendor/arcanely/editor'),
         ], 'editor-resources');
 
         if (function_exists('config_path')) { // function not available and 'publish' not relevant in Lumen
